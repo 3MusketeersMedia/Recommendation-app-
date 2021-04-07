e7c4aabc6270bcf2d977c0f8c4f2ca0e1cecea59
Sri Vishnu Chadalavada <srchadal@ucsc.edu>
2021-04-06 23:07:31 -0700

    sample test edited

--- database/test.py
+++ database/test.py
@@ -8,14 +8,14 @@ exec(open("database.py").read())
 #establish connection
 connection = open_DBConnection()
 
-set_data(connection, "vinu show", "tv show", 1)
-set_data(connection, "vinu show", "movie", 2)
-set_data(connection, "vinu show", "short film", 3)
-set_data(connection, "belen show", "movie", 4)
-set_data(connection, "booby show", "movie", 5)
-set_data(connection, "dooby show", "anime", 6)
+set_data(connection, "v show", "tv show", 1)
+set_data(connection, "v show", "movie", 2)
+set_data(connection, "v show", "short film", 3)
+set_data(connection, "b movie", "movie", 4)
+set_data(connection, "b show", "movie", 5)
+set_data(connection, "d show", "anime", 6)
 
-stuff = get_by_name(connection, "vinu show")
+stuff = get_by_name(connection, "v show")
 print(stuff)
 
 stuff = get_by_id(connection, 6)
