95214f85823b8e5597e26187d6c94a9c15efd5cd
rylee12 <ryanlee7x@gmail.com>
2021-04-14 14:53:54 -0700

    Update 2
    App can send data using simple return function. Next task: grab data from AWS database and reformat to JSON.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,46 @@
-from flask import flask
+import time
 
+#from datetime import datetime
+from flask import Flask
+from flask import redirect, url_for, request
+
+# TA email: yogolan@ucsc.edu
+
+# instance of flask web app
 app = Flask(__name__)
 
-def home():
-    print("home")
+# sample json format
+data = {
+    "person1": {"name": "rick", "age": 16},
+    "person2": {"name": "astley", "age": 65}
+}    
+
+# .\venv\Scripts\activate
+# gives the route to the function
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        # could check the json to determine which function to implement
+        pass
+    return "West virgina Country Roads"
+
+@app.route("/movies", methods=['GET'])
+def movies():
+    return data
+
+@app.route("/search")
+def search():
+    return "search"
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return "login"
+
+@app.route("/time")
+def time():
+    return {"time": time.time()}
+
+if __name__ == "__main__":
+    app.run(debug=True)
