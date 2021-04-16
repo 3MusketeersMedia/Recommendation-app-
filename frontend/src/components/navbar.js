b4bbcf18465233c7170e34165fa3b0bb949b9a52
nickyvitale <nvitale@ucsc.edu>
2021-04-15 17:47:42 -0700

    changed navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -8,12 +8,12 @@ const navbar = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#features">Movie List</Nav.Link>
+      <Nav.Link href="#pricing">Login</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Movie List</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Login</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1">Item 1</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Item 2</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Item 3</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
