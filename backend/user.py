3e2fcec15763f8fb5d49f7c0860f9a115469a4d0
wlphyo <59066863+wlphyo@users.noreply.github.com>
2021-04-16 12:47:33 -0700

    added user.py in progress

--- backend/user.py
+++ backend/user.py
@@ -4,8 +4,30 @@ class User:
         self.password_hash = p_hash
         self.password_salt = p_salt
         self.profile = u_profile
+        self.watched = [] # watched list
+        self.liked = [] # liked list
     def get_username(self):
         return self.username
     def get_password(self):
-        return self.pass
-        
+        pass
+    def get_liked(self):
+        return self.liked
+    def get_watched(self):
+        return self.watched
+
+    def set_liked(self, id, title):
+        self.liked.append((id, title))
+    def set_watched(self, id, title):
+        self.liked.append((id, title))
+
+    def removed_liked(self, id):
+        for i,j in self.liked:
+            if i == id:
+                self.liked.remove((i,j))
+    
+    def removed_watched(self, id):
+        for i,j in self.watched:
+            if i == id:
+                self.watched.remove((i,j))
+
+    
