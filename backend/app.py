e30ff82f92d11ef7489882be88beed6114228c16
rylee12 <ryanlee7x@gmail.com>
2021-04-16 17:09:41 -0700

    Update 3
    Added CORS functionality

--- backend/app.py
+++ backend/app.py
@@ -2,24 +2,21 @@ import time
 import json
 
 from flask import Flask
-from flask import redirect, url_for, request
+from flask import redirect, url_for, request, CORS
 import database
 
 # TA email: yogolan@ucsc.edu
+# .\venv\Scripts\activate
 
 # instance of flask web app
 app = Flask(__name__)
-
-# sample json format
-data = {
-    "person1": {"name": "rick", "age": 16},
-    "person2": {"name": "astley", "age": 65}
-}
+cors = CORS(app)
 
 # items in list are tuples
-# keys of the dictionary will be the movie name
 # json.dumps() converts tuples to arrays
-# all the values in the array are converted to strings apparently - problem?
+# all the values in the array are converted to strings
+# attributes currently: movie name, media type, id
+# structure: {"movie name": (json array of attributes)}
 def format_media(list1):
     json1 = {}
     for item in list1:
@@ -27,10 +24,11 @@ def format_media(list1):
         js_item = json.dumps(item)
         json1[name] = js_item
     
-    #print(json1)
     return json1
 
-# .\venv\Scripts\activate
+def format_user_info():
+    pass
+
 # gives the route to the function
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -41,10 +39,9 @@ def index():
 
 @app.route("/movies", methods=['GET'])
 def movies():
-    # attributes currently: movie name, media type, id
     db_amazon = database.open_DBConnection()
-    hello = database.get_all(db_amazon, "media")
-    dict1 = format_media(hello)
+    all_media = database.get_all(db_amazon, "media")
+    dict1 = format_media(all_media)
     return dict1
 
 # need to know what to search for
