8f875e8eb85285710423f2098160abda219a4169
nickyvitale <nvitale@ucsc.edu>
2021-04-19 10:02:07 -0700

    Homepage basically done for now

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import {AppContext} from '../AppContext';
+import { Link } from 'react-router-dom';
 
 const MyNav = () => {
     return (
-      <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="/">RecomMedia</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/list">Movie List</Nav.Link>
-            <Nav.Link href="#pricing">Login</Nav.Link>
+            <Nav.Link as={Link} to="/list">Movie List</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Item 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Item 2</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Item 3</NavDropdown.Item>
+              <NavDropdown.Item href="/action/3.1">Item 1</NavDropdown.Item>
+              <NavDropdown.Item href="/action/3.2">Item 2</NavDropdown.Item>
+              <NavDropdown.Item href="/action/3.3">Item 3</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="/action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#details">Details</Nav.Link>
-            <Nav.Link eventKey={2} href="#OtherThing">
+            <Nav.Link href="/details">Details</Nav.Link>
+            <Nav.Link eventKey={2} href="/OtherThing">
               Other Thing
             </Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Navbar>
-      </div>
     );
 }
 
