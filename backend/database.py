675a8090d2fcf33ae029a823ce53a7f1a7f8dd5c
Sri Vishnu Chadalavada <srchadal@ucsc.edu>
2021-04-21 13:00:55 -0700

    can store reviews and summaries now

--- backend/database.py
+++ backend/database.py
@@ -12,7 +12,7 @@ if conn is None:
     exit()
 
 #make tables
-database.execute("CREATE TABLE IF NOT EXISTS media(name VARCHAR NOT NULL, mediaType VARCHAR NOT NULL, year INT, link VARCHAR, genres VARCHAR, rating NUMERIC, running_time NUMERIC, ID VARCHAR, PRIMARY KEY(ID));")
+database.execute("CREATE TABLE IF NOT EXISTS media(name VARCHAR NOT NULL, mediaType VARCHAR NOT NULL, year INT, link VARCHAR, genres VARCHAR, rating NUMERIC, running_time NUMERIC, summary VARCHAR, ID VARCHAR, PRIMARY KEY(ID));")
 
 database.execute("CREATE TABLE IF NOT EXISTS users(username VARCHAR NOT NULL, password_hash VARCHAR NOT NULL, password_salt VARCHAR NOT NULL, ID VARCHAR, PRIMARY KEY(ID));")
 
@@ -56,45 +56,45 @@ def add_user(pair, username, password_hash, password_salt, user_id):
             #insert if false
 
 
-def set_data(pair, name, mediaType, year, link, genres, rating, running_time, ID):
+def set_data(pair, name, mediaType, year, link, genres, rating, running_time, ID, summary="None"):
     #retrieve list of ID's
     pair[1].execute("SELECT ID FROM media WHERE ID = '{}';".format(ID))
     list_id = pair[1].fetchall()
     #check for ID
     if pair[2] == False:
         if (ID,) in list_id:
-            pair[1].execute("UPDATE media SET name = '{}', mediaType = '{}', year = {}, link = '{}', genres = '{}', rating = {}, running_time = {} WHERE ID = '{}';".format(name, mediaType, year, link, genres, rating, running_time, ID))
+            pair[1].execute("UPDATE media SET name = '{}', mediaType = '{}', year = {}, link = '{}', genres = '{}', rating = {}, running_time = {}, summary = '{}' WHERE ID = '{}';".format(name, mediaType, year, link, genres, rating, running_time, summary, ID))
             #update if true
         else:
-            pair[1].execute("INSERT INTO media VALUES('{}', '{}', {}, '{}', '{}', {}, {}, '{}');".format(name, mediaType, year, link, genres, rating, running_time, ID))
+            pair[1].execute("INSERT INTO media VALUES('{}', '{}', {}, '{}', '{}', {}, {}, '{}', '{}');".format(name, mediaType, year, link, genres, rating, running_time, summary, ID))
             #insert if false
     else:
         if len(list_id) > 0 and ID == list_id[0]['id']:
-            pair[1].execute("UPDATE media SET name = '{}', mediaType = '{}', year = {}, link = '{}', genres = '{}', rating = {}, running_time = {} WHERE ID = '{}';".format(name, mediaType, year, link, genres, rating, running_time, ID))
+            pair[1].execute("UPDATE media SET name = '{}', mediaType = '{}', year = {}, link = '{}', genres = '{}', rating = {}, running_time = {}, summary = '{}' WHERE ID = '{}';".format(name, mediaType, year, link, genres, rating, running_time, summary, ID))
             #update if true
         else:
-            pair[1].execute("INSERT INTO media VALUES('{}', '{}', {}, '{}', '{}', {}, {}, '{}');".format(name, mediaType, year, link, genres, rating, running_time, ID))
+            pair[1].execute("INSERT INTO media VALUES('{}', '{}', {}, '{}', '{}', {}, {}, '{}', '{}');".format(name, mediaType, year, link, genres, rating, running_time, summary, ID))
             #insert if false
 
 
-def set_user_data(pair, table, watched, liked, ID, rating="NULL"):
+def set_user_data(pair, table, watched, liked, ID, rating="NULL", review=" "):
     #retrieve list of ID's
     pair[1].execute("SELECT ID FROM {} WHERE ID = '{}';".format(table, ID))
     list_id = pair[1].fetchall()
     #check for ID
     if pair[2] == False:
         if (ID,) in list_id:
-            pair[1].execute("UPDATE {} SET watched = {}, liked = {}, rating = {} WHERE ID = '{}';".format(table, watched, liked, rating, ID))
+            pair[1].execute("UPDATE {} SET watched = {}, liked = {}, rating = {}, review = '{}' WHERE ID = '{}';".format(table, watched, liked, rating, review, ID))
             #update if true
         else:
-            pair[1].execute("INSERT INTO {} VALUES({}, {}, {}, '{}');".format(table, watched, liked, rating, ID))
+            pair[1].execute("INSERT INTO {} VALUES({}, {}, {}, '{}', '{}');".format(table, watched, liked, rating, review, ID))
             #insert if false
     else:
         if len(list_id) > 0 and ID == list_id[0]['id']:
-            pair[1].execute("UPDATE {} SET watched = {}, liked = {}, rating = {} WHERE ID = '{}';".format(table, watched, liked, rating, ID))
+            pair[1].execute("UPDATE {} SET watched = {}, liked = {}, rating = {}, review = '{}' WHERE ID = '{}';".format(table, watched, liked, rating, review, ID))
             #update if true
         else:
-            pair[1].execute("INSERT INTO {} VALUES({}, {}, {}, '{}');".format(table, watched, liked, rating, ID))
+            pair[1].execute("INSERT INTO {} VALUES({}, {}, {}, '{}', '{}');".format(table, watched, liked, rating, review, ID))
             #insert if false
 
 
@@ -193,7 +193,7 @@ def delete_table(pair, table):
 
 
 def create_user_table(pair, user):
-    pair[1].execute("CREATE TABLE IF NOT EXISTS {}(watched BOOLEAN NOT NULL, liked BOOLEAN NOT NULL, rating NUMERIC, ID VARCHAR, FOREIGN KEY (ID) REFERENCES media (ID));".format(user))
+    pair[1].execute("CREATE TABLE IF NOT EXISTS {}(watched BOOLEAN NOT NULL, liked BOOLEAN NOT NULL, rating NUMERIC, review VARCHAR, ID VARCHAR, FOREIGN KEY (ID) REFERENCES media (ID));".format(user))
 
 
 def clear_data(pair, table):
