7de6b409ac714eeb9bdf53632d53c7358c0f05c3
Jeffrey Wang <wangj2840@gmail.com>
2021-05-03 14:08:38 -0700

    Added Documentation

--- frontend/src/AppContext.js
+++ frontend/src/AppContext.js
@@ -2,10 +2,27 @@ import React, { useState, useEffect } from "react";
 
 export const AppContext = React.createContext();
 
-// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
+/** App Context Template taken from WebApp boilerplate with React JS
+ *  Read more here to learn how the template can be applied https://github.com/4GeeksAcademy/react-hello-webapp/tree/master/src/js/store
+ * 
+ *  ContextWrapper provides global variables and methods in a single Context. It does so by intializing a state (getState) where you can inject
+ *  and intialize different sets of context into a single state (see below) and provides certain functions to get and update them. 
+ *  It then wraps the context in APP.js by taking all the child components in APP.js passed as {children} and passing it as subscribers of 
+ *  Appcontext (the context we are using). 
+ * 
+ *  Use Cases: 
+ *
+ *   -> Classes: simply import AppContext to componenets and set ContextType to it. 
+ *              -> To access variables use this this.context.store.<varType>
+ *              -> To access action use this this.context.action.<actionType>
+ * 
+ *   -> Hooks: Use import {state, actions} from Appcontext 
+ *              -> To access variables or actions, simply append the type you want. (i.e. action.setMovies(), store.token)
+ * 
+ * It's preferred to use this Context when setting global variables and fetching from back up. Data shared here will be available to any 
+ * Components inside App.js
+*/
 const ContextWrapper = ({children}) => {
-    //this will be passed as the contenxt value
     const [state, setState] = useState(
         getState({
             getStore: () => state.store,
@@ -42,6 +59,15 @@ const ContextWrapper = ({children}) => {
 
 export default ContextWrapper;
 
+
+/** getState - Stores the global variables and methods in a single Context. You can also use the props in
+ *             other to access other states in the Context: 
+ *           -> getStore(): gets the current value of store in the context. 
+ *           -> get Actions: Calls an action in the current context 
+ *           -> setStore(): Updates the store in the variable 
+ * 
+ * @returns States that will be used to create a context in ContextWrapper (see above) 
+*/
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -49,14 +75,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 			token: null,
 		},
 		actions: {
+            /** */
             syncToken: async () => {
                 const token = await sessionStorage.getItem("token");
                 if(token && token != "" && token != undefined) 
                     setStore({token: token});
             }, 
 
+            /**Login checks new  and returns token 
+             * @return Currently returns  TRUE/FALSE whether methods is successful 
+             * ---> May need to change return type for rendering conditions in 
+             *      login Component   
+            */
             login: async (username, password) => {
-                console.log(username,  password)
+
                 const args = ({
                     method: "POST", 
                     headers: {
@@ -64,16 +96,14 @@ const getState = ({ getStore, getActions, setStore }) => {
                     }, 
                     body: JSON.stringify({username, password}),
                 });
-                const response = await fetch('http://localhost:5000/login',args);
-                const data = await response.json(); 
+
                 try{
-                    console.log(args) 
                     const response = await fetch('http://localhost:5000/login',args);
                     const data = await response.json(); 
                     console.log(data);
                     
                     if(response.status !== 200){
-                        console.log("Invalid"); 
+                        console.log("Status Code: " + response.status); 
                         return false; 
                     } 
                     
@@ -82,22 +112,27 @@ const getState = ({ getStore, getActions, setStore }) => {
                     return true;
                 }
                 catch(error){
-                    console.log("Invalid");
+                    console.log("Login connection dropped");
                 }
             }, 
 
 
+            /** Simply removes token from sessionStorage*/
             logout: async () => {
                 const token = sessionStorage.remove("token");
                 setStore({token: null});
             }, 
 
 
-            /**Signup takes a new set of user/pass and returns token 
+            /**Signup takes a new set of user/pass and stores the new token
+             * @return Currently returns  TRUE/FALSE whether methods is successful 
              * ---> May need to change for some sort of email validation check.
-             *      to prevent botting.  
+             *      to prevent botting.
+             * ---> May need to change return type for rendering conditions in 
+             *      login Component   
             */
             signup: async (username, password) => {
+
                 const args = ({
                     method: "POST", 
                     headers: {
@@ -105,13 +140,14 @@ const getState = ({ getStore, getActions, setStore }) => {
                     }, 
                     body: JSON.stringify({username, password}),
                 });
+
                 try{
                     const response = await fetch('http://localhost:5000/signup', {args})
                     const data = await response.json(); 
                     console.log(data);
                     
                     if(response.status !== 200){
-                        console.log("Invalid"); 
+                        console.log("Status Code: " + response.status); 
                         return false; 
                     } 
                     
@@ -119,15 +155,22 @@ const getState = ({ getStore, getActions, setStore }) => {
                     return true;
                 }
                 catch(error){
-                    console.log(error);
+                    console.log("Signup connection dropped");
                 }
             }, 
 
+            /** Changes the current movie in route /movies by modifying 
+             * "movie" in the store state and localstorage. 
+            */
             setMovie: async(movie) => {
                 localStorage.setItem('movie', JSON.stringify(movie));
                 await setStore({movie: movie});
             },
 
+            /** Ensures that the local storage value of "movie" is 
+             *  loaded into store.movie. 
+             *  --> currently called by zoomed-page
+            */
             syncMovies: async() =>{
                 const movie = JSON.parse(localStorage.getItem('movie'));
                 await setStore({movie: movie});
