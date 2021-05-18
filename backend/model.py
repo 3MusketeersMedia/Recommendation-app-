6931b77d025ceb6905c5468ea946c21e5378677e
Sri Vishnu Chadalavada <srchadal@ucsc.edu>
2021-05-17 17:18:35 -0700

    merged updated algorithm

--- backend/model.py
+++ backend/model.py
@@ -70,37 +70,50 @@ def search_media_table(pair, query):
     return end
 
 
-def advanced_search_media_table(pair, query, mediaType, genre, yearStart, ratingMin, yearEnd=-1, ratingMax=-1):
-    
-    if yearEnd == -1:
-        yearEnd = yearStart
-    if ratingMax == -1:
-        ratingMax = ratingMin
+def advanced_search_media_table(pair, query, genre, yearStart, ratingMin, yearEnd, ratingMax):
+    # Checks invalid input, returns empty list if they give some stupid input
+    yearStartInvalid = (yearStart != None and not yearStart.isnumeric() and yearStart.strip() != "")
+    yearEndInvalid = (yearEnd != None and not yearEnd.isnumeric() and yearEnd.strip() != "")
+    ratingMinInvalid = (ratingMin != None and not ratingMin.isnumeric() and ratingMin.strip() != "")
+    ratingMaxInvalid = (ratingMax != None and not ratingMax.isnumeric() and ratingMax.strip() != "")
+    if (yearStartInvalid or yearEndInvalid or ratingMinInvalid or ratingMaxInvalid):
+        return []
 
-    #filter
-    filtered_query = filter(query)
-    tmp2 = "%" + mediaType + "%"
-    pair[1].execute("SELECT * FROM media WHERE name LIKE %s AND year >= %s AND year <= %s AND rating >= %s AND rating <= %s AND mediaType LIKE %s;", (query, yearStart, yearEnd, ratingMin, ratingMax, tmp2))
-    exact = pair[1].fetchall()
+    # Checks for empty string being passed in (BTW, default args don't work since it's not NOTHING that's being passed in, it's an empty string--> doesn't default)
+    if yearStart == "" or yearStart == None:
+        yearStart = "0"
+    if yearEnd == "" or yearEnd == None:
+        yearEnd = "2050"
+    if ratingMin == "" or ratingMin == None:
+        ratingMin = "0"
+    if ratingMax == "" or ratingMax == None:
+        ratingMax = "10"
 
-    #get list, we are going to add the exact match at the end as the first result
-    #so make sure that it isnt the same movie as exact match
     results = []
-    for i in filtered_query:
-        tmp = "%" + i + "%"
-        if len(exact) > 0:
-            pair[1].execute("SELECT * FROM media WHERE name LIKE %s AND ID <> %s AND year >= %s AND year <= %s AND rating >= %s AND rating <= %s AND mediaType LIKE %s;", (tmp, exact[0][8], yearStart, yearEnd, ratingMin, ratingMax, tmp2))
+    if(query != "" and query != None):
+        filtered_query = filter(query.strip())
+        if(genre != "" and genre != None):
+            tmp2 = "%" + genre.lower().strip() + "%"
+            for i in filtered_query:
+                tmp = "%" + i + "%"
+                pair[1].execute("SELECT * FROM media WHERE LOWER(name) LIKE %s AND year >= %s AND year <= %s AND rating >= %s AND rating <= %s AND LOWER(genres) LIKE %s;", (tmp, yearStart, yearEnd, ratingMin, ratingMax, tmp2))
+                results += pair[1].fetchall()
         else:
-            pair[1].execute("SELECT * FROM media WHERE name LIKE %s AND year >= %s AND year <= %s AND rating >= %s AND rating <= %s AND mediaType LIKE %s;", (tmp, yearStart, yearEnd, ratingMin, ratingMax, tmp2))
-        results += pair[1].fetchall()
-
+            for i in filtered_query:
+                tmp = "%" + i + "%"
+                pair[1].execute("SELECT * FROM media WHERE LOWER(name) LIKE %s AND year >= %s AND year <= %s AND rating >= %s AND rating <= %s;", (tmp, yearStart, yearEnd, ratingMin, ratingMax))
+                results += pair[1].fetchall()
+    else:
+        if(genre != "" and genre != None):
+            tmp2 = "%" + genre.strip() + "%"
+            pair[1].execute("SELECT * FROM media WHERE year >= %s AND year <= %s AND rating >= %s AND rating <= %s AND LOWER(genres) LIKE %s;", (yearStart, yearEnd, ratingMin, ratingMax, tmp2))
+            results += pair[1].fetchall()
+        else:
+            pair[1].execute("SELECT * FROM media WHERE year >= %s AND year <= %s AND rating >= %s AND rating <= %s;", (yearStart, yearEnd, ratingMin, ratingMax))
+            results += pair[1].fetchall()
+    
     #sort list by frequency of tuple
-    end = [key for key, value in collections.Counter(results).most_common()]
-
-    #add exact match as first result if it exists
-    if len(exact) > 0:
-        end = exact + end 
-    return end
+    return [key for key, value in collections.Counter(results).most_common()]
 
 
 def get_user_recommendations(pair, user_id):
